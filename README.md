719060df81844a26f0e4d7212147131f1b9db20f
Farhan <you@example.comfmuh8402@gmail.com>
2024-07-30 21:20:02 +0800

    feat: get user info

---
+++ README.md
@@ -0,0 +1,20 @@
+```
+npm install
+npm run dev
+```
+
+```
+open http://localhost:3000
+```
+
+- Production: `https://projectname.yourname.com`
+- Local: `http://localhost:3000`
+
+| Endpoint     | HTTP     | Description       |
+| ------------ | -------- | ----------------- |
+| `/users`     | `GET`    | Get all items     |
+| `/users/:id` | `GET`    | Get item by id    |
+| `/users`     | `POST`   | Add new item      |
+| `/users`     | `DELETE` | Delete all items  |
+| `/users/:id` | `DELETE` | Delete item by id |
+| `/users/:id` | `PUT`    | Update item by id |
---
+++ src/index.tsx
@@ -0,0 +1,70 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
+
+const app = new Hono()
+
+
+const users = [
+  {
+    id:1,
+    name: 'farhan',
+    addres: 'sungai kunyit',
+    heigth: 160,
+    weight: 60
+  },
+  {
+    id:2,
+    name: 'ibnu',
+    addres: 'parit banjar',
+    heigth: 160,
+    weight: 60
+  },
+  {
+    id:3,
+    name: 'aini',
+    addres: 'malikian',
+    heigth: 100,
+    weight: 40
+  }
+]
+
+app.get('/', (c) => {
+  return c.text('Halo kamu ada di aplikasi saya')
+});
+
+app.get('/users', (c) => {
+  return c.json(users)
+});
+
+app.get("/users/:id", (c) => {
+  const idReq = parseInt(c.req.param("id"), 10);
+  const founder = users.find(user => user.id === idReq);
+
+  if (!founder) {
+    return c.json({message: 'user not found'}, 404)
+  }
+
+  return c.json(founder);
+});
+
+
+// basic auth
+app.use('/admin/*',
+  basicAuth({
+    username: 'admin',
+    password: 'secret',
+  })
+);
+
+app.get('/admin', (c) => {
+  return c.text('You are authorized!')
+})
+
+const port: number = 3000;
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
