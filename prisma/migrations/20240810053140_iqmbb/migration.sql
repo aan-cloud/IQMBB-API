5fa73875c75b75e52b9fd51a6aaf4d6bd53e9c4f
Muhammad Farhan <evirsanti@gmail.com>
2024-08-10 13:37:47 +0800

    chore: add prisma model

---
+++ prisma/migrations/20240810053140_iqmbb/migration.sql
@@ -0,0 +1,62 @@
+-- CreateTable
+CREATE TABLE "operator" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "addres" TEXT NOT NULL,
+    "healty" BOOLEAN NOT NULL,
+
+    CONSTRAINT "operator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "section" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "section_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "machine" (
+    "id" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+
+    CONSTRAINT "machine_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tube" (
+    "id" TEXT NOT NULL,
+    "part" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+
+    CONSTRAINT "tube_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "scedule" (
+    "id" TEXT NOT NULL,
+    "operator" TEXT NOT NULL,
+    "section" TEXT NOT NULL,
+    "machine" TEXT NOT NULL,
+    "tube" TEXT NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "scedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "section_name_key" ON "section"("name");
+
+-- AddForeignKey
+ALTER TABLE "scedule" ADD CONSTRAINT "scedule_operator_fkey" FOREIGN KEY ("operator") REFERENCES "operator"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "scedule" ADD CONSTRAINT "scedule_section_fkey" FOREIGN KEY ("section") REFERENCES "section"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "scedule" ADD CONSTRAINT "scedule_machine_fkey" FOREIGN KEY ("machine") REFERENCES "machine"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "scedule" ADD CONSTRAINT "scedule_tube_fkey" FOREIGN KEY ("tube") REFERENCES "tube"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
